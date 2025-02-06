11b3dc59467e43ae358ebdfa33984ddc646f6728
KyleRobertson22 <robertsonkyle523@gmail.com>
2025-02-06 16:23:44 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -3,3 +3,8 @@ html_theme_path = []
 html_static_path = ["_static"]  # For custom CSS, images, JS
 templates_path = ["_templates"]
 html_sidebars = {}  # Disable sidebar
+html_context = {
+    "readthedocs_show_version_warning": False,  # Disable version warning
+    "readthedocs_add_google_analytics": False,  # Disable analytics (optional)
+    "readthedocs_disable_flyout": True,         # Disable the flyout menu
+}
