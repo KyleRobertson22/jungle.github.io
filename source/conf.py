4ca0076819db06c9b40307f072afe60ba74a23ad
KyleRobertson22 <robertsonkyle523@gmail.com>
2025-02-06 15:57:46 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -2,4 +2,5 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 extensions = ['sphinx.ext.autodoc', 'myst_parser']
-html_theme = 'alabaster'
+html_theme = "basic"
+html_static_path = ["_static"]
