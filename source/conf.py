d5e63a7af95d3960571503fdf4a737feb3c3d025
KyleRobertson22 <robertsonkyle523@gmail.com>
2025-02-06 16:01:49 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-extensions = ['sphinx.ext.autodoc', 'myst_parser']
+extensions = ['myst_parser']
 html_theme = "basic"
 html_static_path = ["_static"]
